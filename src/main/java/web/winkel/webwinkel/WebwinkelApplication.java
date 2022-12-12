8d43ab1a10cd6194c68ec92200fa8d1063f57f73
Karst Backer <karst@makajelaan.nl>
2022-12-12 16:18:40 +0100

    create dummy api

--- src/main/java/web/winkel/webwinkel/WebwinkelApplication.java
+++ src/main/java/web/winkel/webwinkel/WebwinkelApplication.java
@@ -2,7 +2,11 @@ package web.winkel.webwinkel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class WebwinkelApplication {
 
@@ -10,4 +14,8 @@ public class WebwinkelApplication {
 		SpringApplication.run(WebwinkelApplication.class, args);
 	}
 
+    @GetMapping()
+	public String getAppleBags(@RequestParam(value = "name", defaultValue = "world") String name) {
+		return "Hello " + name;
+	}
 }
